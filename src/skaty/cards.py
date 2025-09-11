f4075d1034d88b4b8f441f9e7ea9f49ff056602d
Luis <luis@l16r.com>
2025-09-11 14:33:05 +0200

    feat(cards): add deck, restructure Value as Rank
    The previous Value was an enum with the associated points. This can lead
    to errors if iterating over the enum without .__members__ call. Thus,
    implement Rank Enum with associated property points.

--- src/skaty/cards.py
+++ src/skaty/cards.py
@@ -1,19 +1,33 @@
-from enum import Enum
+import random
+from enum import Enum, IntEnum
 
 
-class Value(Enum):
-    """
-    ISkO 1.2.2.
-    """
-
-    SEVEN = 0
-    EIGHT = 0
-    NINE = 0
-    JACK = 2
-    QUEEN = 3
-    KING = 4
+class Rank(IntEnum):
+    # Using rank values to differentiate between values
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
     TEN = 10
-    ACE = 11
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
+
+    @property
+    def points(self) -> int:
+        """
+        Returns the point value of the card rank in Skat (ISkO 1.2.2).
+        """
+        if self in (Rank.TEN, Rank.KING):
+            return 10
+        elif self == Rank.ACE:
+            return 11
+        elif self == Rank.QUEEN:
+            return 3
+        elif self == Rank.JACK:
+            return 2
+        # 7,8,9
+        return 0
 
 
 class Suit(Enum):
@@ -21,26 +35,54 @@ class Suit(Enum):
     ISkO 1.2.1.
     """
 
-    DIAMONDS = 9
-    HEARTS = 10
-    SPADES = 11
-    CLUBS = 12
+    DIAMONDS = 0
+    HEARTS = 1
+    SPADES = 2
+    CLUBS = 3
 
 
 class Card:
-    def __init__(self, value: Value, suit: Suit) -> None:
-        self._value = value
+    def __init__(self, rank: Rank, suit: Suit) -> None:
+        self._rank = rank
         self._suit = suit
 
     @property
-    def value(self) -> Value:
-        return self._value
+    def rank(self) -> Rank:
+        return self._rank
 
     @property
     def suit(self) -> Suit:
         return self._suit
 
+    @property
+    def points(self) -> int:
+        return self._rank.points
+
     def __eq__(self, other) -> bool:
         if not isinstance(other, Card):
             return False
-        return self.suit is other.suit and self.value is other.value
+        return self.suit is other.suit and self.rank is other.rank
+
+    def __str__(self) -> str:
+        return f"{self.rank.name} of {self.suit.name}"
+
+    def __repr__(self) -> str:
+        return f"Card({self.rank.name}, {self.suit.name})"
+
+    def __hash__(self):
+        return hash((self.suit, self.rank))
+
+
+def create_deck() -> list[Card]:
+    """
+    Creates an unshuffled Skat deck.
+    """
+    deck = list()
+    for suit in Suit:
+        for rank in Rank:
+            deck.append(Card(rank, suit))
+    return deck
+
+
+def shuffle_deck(deck: list[Card]) -> list[Card]:
+    return random.sample(deck, len(deck))
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,12 +1,36 @@
-from skaty.cards import Card, Suit, Value
+from skaty.cards import Card, Rank, Suit, create_deck, shuffle_deck
 
 
 def test_eq():
     non_card = object()
-    c1 = Card(Value.ACE, Suit.SPADES)
-    c2 = Card(Value.JACK, Suit.CLUBS)
-    c3 = Card(Value.JACK, Suit.CLUBS)
+    c1 = Card(Rank.ACE, Suit.SPADES)
+    c2 = Card(Rank.JACK, Suit.CLUBS)
+    c3 = Card(Rank.JACK, Suit.CLUBS)
 
     assert c1 != c2
     assert c2 == c3
     assert c1 != non_card
+
+
+def test_create_deck():
+    deck = create_deck()
+    # A Skat deck contains exactly 32 cards
+    assert len(deck) == 32
+    # Check for unique cards
+    assert len(set(deck)) == 32
+
+
+def test_shuffle_deck():
+    deck = create_deck()
+    shuffled = shuffle_deck(deck)
+    # All cards must still be present and unique
+    assert len(shuffled) == len(deck)
+    assert set(shuffled) == set(deck)
+
+    # Test how many cards are still in place (flaky). This could be done better by shuffling multiple times and measuring the shift.
+    in_place = 0
+    for i in range(len(deck)):
+        if deck[i] == shuffled[i]:
+            in_place += 1
+    # At least 7 cards should move
+    assert in_place < len(deck) - 6
