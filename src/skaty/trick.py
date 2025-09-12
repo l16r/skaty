f760f444b6f2914056e3dca99edd18dc4b64a0d5
Luis <luis@l16r.com>
2025-09-12 14:18:56 +0200

    docs(trick): add documentation

--- src/skaty/trick.py
+++ src/skaty/trick.py
@@ -2,7 +2,6 @@ from functools import reduce
 
 from skaty.cards import Card
 from skaty.exceptions import InvalidGameStateError
-from skaty.player import Player
 from skaty.rules import AbstractRuleSet
 
 
@@ -21,13 +20,17 @@ class Trick:
     def is_complete(self):
         return len(self._cards) == 3
 
+    # TODO: test
     def get_winner(self, rule_set: AbstractRuleSet) -> int:
+        """
+        Get the index of the winner in the trick in the order the trick was played.
+        """
         if not self.is_complete():
             raise InvalidGameStateError("Trick not complete. Cannot calculate winner.")
         return rule_set.determine_trick_winner(self._cards)
 
     def getTrickPoints(self) -> int:
         """
-        Returns the sum of all card points.
+        Returns the sum of all card points. The trick does not have to be completed.
         """
         return reduce(lambda acc, c: acc + c.points, self._cards, 0)
