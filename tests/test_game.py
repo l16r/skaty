06865a36cbdc820e580c13e840b1803789e2e736
Luis <luis@l16r.com>
2025-12-25 18:16:17 +0100

    test(game): add test for a valid game

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,9 +1,12 @@
+from typing import Optional
 from pytest import raises
+from skaty.cards import Card
 from skaty.exceptions import InvalidActionError, InvalidGameStateError
 from skaty.game_state import GameState
 from skaty.isko import ISkO
 from skaty.player import Player
 from skaty.rules import (
+    AbstractRuleSet,
     BurySkat,
     DealCards,
     DeclareBid,
@@ -14,6 +17,14 @@ from skaty.rules import (
     Pass,
     PlayCard,
 )
+
+
+def any_valid_card(player: Player, rules: AbstractRuleSet, first_card: Optional[Card]):
+    print(f"{first_card=}")
+    for c in player.hand:
+        if rules.is_valid_card_play(player, c, first_card):
+            return c
+    return player.hand[0]
 
 
 def test_valid_game():
@@ -36,4 +47,25 @@ def test_valid_game():
         game.apply_action(p2, DeclareGame(GameType.CLUBS, False))
     game.apply_action(p2, BurySkat((p2.hand[0], p2.hand[1])))
     game.apply_action(p2, DeclareGame(GameType.GRAND, False))
-    game.apply_action(p0, PlayCard(p0.hand[0]))
+    active_player = 0
+    for _ in range(10):
+        first_card = game._players[active_player].hand[0]
+        game.apply_action(
+            game._players[active_player], PlayCard(game._players[active_player].hand[0])
+        )
+        active_player = game._active_player
+        game.apply_action(
+            game._players[active_player],
+            PlayCard(
+                any_valid_card(game._players[active_player], game._rule_set, first_card)
+            ),
+        )
+        active_player = game._active_player
+        game.apply_action(
+            game._players[active_player],
+            PlayCard(
+                any_valid_card(game._players[active_player], game._rule_set, first_card)
+            ),
+        )
+        active_player = game._active_player
+        first_card = None
